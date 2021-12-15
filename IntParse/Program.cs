1e20ca5f3be5522c4c16da2f7fa1e97fe0c60eab
mc0re <mnikitin0@gmail.com>
2021-12-15 20:48:57 +0100

    Transfer code from PC.

---
+++ IntParse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+var input = Console.ReadLine();
+if (int.TryParse(input, out var n))
+    Console.WriteLine($"Input: {n}");
+else
+    throw new ArgumentException($"Wrong input '{input}'.");
+return;
+
+
+//var n = 0;
+
+foreach (var ch in input)
+{
+    if (char.IsDigit(ch))
+        n = n * 10 + (ch - '0');
+    else
+        throw new ArgumentException($"Unexpected character {ch}.");
+}
+
+Console.WriteLine($"Input: {n}");
